e62b4e70ce0320401dc0ada593e4275d7e77cbd6
eliassjogreen <eliassjogreen1@gmail.com>
2020-03-06 18:38:48 +0100

    Added webview_run function

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,50 @@
 #[macro_use]
 extern crate deno_core;
 extern crate futures;
+extern crate serde;
+extern crate serde_json;
 
 use deno_core::CoreOp;
 use deno_core::Op;
 use deno_core::PluginInitContext;
-use deno_core::{Buf, ZeroCopyBuf};
-use futures::future::FutureExt;
+use deno_core::ZeroCopyBuf;
+use serde::{Deserialize, Serialize};
+use web_view::*;
+
+#[derive(Serialize, Deserialize)]
+struct Options {
+  title: String,
+  width: i32,
+  height: i32,
+  resizable: bool,
+  debug: bool,
+  content: String,
+}
 
 fn init(context: &mut dyn PluginInitContext) {
-  context.register_op("testSync", Box::new(op_test_sync));
-  context.register_op("testAsync", Box::new(op_test_async));
+  context.register_op("webview_run", Box::new(op_webview_run));
 }
 init_fn!(init);
 
-pub fn op_test_sync(data: &[u8], zero_copy: Option<ZeroCopyBuf>) -> CoreOp {
-  if let Some(buf) = zero_copy {
-    let data_str = std::str::from_utf8(&data[..]).unwrap();
-    let buf_str = std::str::from_utf8(&buf[..]).unwrap();
-    println!(
-      "Hello from plugin. data: {} | zero_copy: {}",
-      data_str, buf_str
-    );
-  }
-  let result = b"test";
-  let result_box: Buf = Box::new(*result);
-  Op::Sync(result_box)
+pub fn op_webview_run(data: &[u8], zero_copy: Option<ZeroCopyBuf>) -> CoreOp {
+  webview_run(std::str::from_utf8(data).unwrap()).unwrap();
+
+  Op::Sync(Box::new([0]))
 }
 
-pub fn op_test_async(data: &[u8], zero_copy: Option<ZeroCopyBuf>) -> CoreOp {
-  let data_str = std::str::from_utf8(&data[..]).unwrap().to_string();
-  let fut = async move {
-    if let Some(buf) = zero_copy {
-      let buf_str = std::str::from_utf8(&buf[..]).unwrap();
-      println!(
-        "Hello from plugin. data: {} | zero_copy: {}",
-        data_str, buf_str
-      );
-    }
-    let (tx, rx) = futures::channel::oneshot::channel::<Result<(), ()>>();
-    std::thread::spawn(move || {
-      std::thread::sleep(std::time::Duration::from_secs(1));
-      tx.send(Ok(())).unwrap();
-    });
-    assert!(rx.await.is_ok());
-    let result = b"test";
-    let result_box: Buf = Box::new(*result);
-    Ok(result_box)
-  };
+fn webview_run<'a>(data: &'a str) -> Result<(), serde_json::Error> {
+  let options: Options = serde_json::from_str(data)?;
 
-  Op::Async(fut.boxed())
+  let webview = web_view::builder()
+    .title(&options.title)
+    .content(Content::Html(options.content))
+    .size(options.width, options.height)
+    .resizable(options.resizable)
+    .debug(options.debug)
+    .user_data(())
+    .invoke_handler(|_webview, _arg| Ok(()))
+    .build()
+    .unwrap();
+  webview.run().unwrap();
+  Ok(())
 }
