9ee629e1b23de6f1a24be623ca73ce58a9305133
eliassjogreen <eliassjogreen1@gmail.com>
2020-03-14 19:50:09 +0100

    Fixed build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,7 +3,7 @@ export async function build(edge: boolean = Deno.args[0] === "edge") {
 
     // TODO (#3) fix
     if (edge) {
-        command.push("--features edge");
+        command.push("--features", "edge");
     }
 
     const cargo = Deno.run({
