432a7e8260eccab99a33bef8021a816685d1a2c5
Divy Srivastava <dj.srivastava23@gmail.com>
2020-05-24 11:24:41 +0530

    feat: point url to latest releases

--- plugin.ts
+++ plugin.ts
@@ -1,7 +1,7 @@
 import { prepare, deferred } from "./deps.ts";
 
 export const PLUGIN_URL_BASE = Deno.env.get("DENO_WEBVIEW_PLUGIN_BASE") ||
-  "https://github.com/eliassjogreen/deno_webview/releases/download/0.4.1";
+  "https://github.com/eliassjogreen/deno_webview/releases/latest/download";
 const PLUGIN_URL = Deno.env.get("DENO_WEBVIEW_PLUGIN");
 const DEBUG = Boolean(Deno.env.get("DENO_WEBVIEW_DEBUG"));
 
