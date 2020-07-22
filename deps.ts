315ef7707dd6d269b3a1fc82e118a8c0221cbecd
Qu4k <pippo.reds@gmail.com>
2020-07-22 17:46:14 +0200

    chore: release 0.4.5

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Plug } from "https://x.nest.land/plug@0.0.3/mod.ts";
+export { Plug } from "https://x.nest.land/plug@0.0.4/mod.ts";
 export { deferred } from "https://deno.land/std@0.61.0/async/mod.ts";
