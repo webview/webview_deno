16de8da2b0529fb3c9de43ae2f4186946e5fb1eb
eliassjogreen <eliassjogreen1@gmail.com>
2020-09-19 22:31:30 +0200

    upgrade: plug dependency

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Plug } from "https://deno.land/x/plug@0.2.1/mod.ts";
+export { Plug } from "https://deno.land/x/plug@0.2.2/mod.ts";
 export { deferred } from "https://deno.land/std@0.70.0/async/mod.ts";
