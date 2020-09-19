8b2fb913ae7abab5ea955f20d66d886352e29fa3
eliassjogreen <eliassjogreen1@gmail.com>
2020-09-19 22:30:19 +0200

    fix: worker example

--- examples/worker/runner.ts
+++ examples/worker/runner.ts
@@ -1,4 +1,9 @@
-const worker = new Worker("./worker.ts", { type: "module", deno: true });
+import { join, dirname } from "https://deno.land/std/path/mod.ts";
+
+const worker = new Worker(
+  join(dirname(import.meta.url), "worker.ts"),
+  { type: "module", deno: true },
+);
 
 // Current problem is that it blocks recieving messages in the worker
 worker.postMessage("close");
