c13741df26aad97a0e7ee8dc7064c8652e01058a
eliassjogreen <eliassjogreen1@gmail.com>
2020-12-01 19:23:02 +0100

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Plug } from "https://deno.land/x/plug@0.2.5/mod.ts";
+export { Plug } from "https://deno.land/x/plug@0.2.6/mod.ts";
 export { deferred } from "https://deno.land/std@0.79.0/async/mod.ts";
