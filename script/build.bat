df9fe3a70158ed62ba6c4d7aa0d58d4d53cf9e2c
eliassjogreen <eliassjogreen1@gmail.com>
2022-06-25 02:06:13 +0200

    upload whole build catalog as artifacts

--- script/build.bat
+++ script/build.bat
@@ -4,6 +4,7 @@ echo Prepare directories...
 set script_dir=%~dp0
 set src_dir=%script_dir%..\webview
 set build_dir=%script_dir%..\build
+mkdir "%build_dir%"
 
 echo Webview directory: %src_dir%
 echo Build directory: %build_dir%
