ca5a7a7b72f8e8d1938916e7b1c5a26f71865f9c
Snider <snider@lt.hn>
2022-06-25 14:24:33 +0100

    restoring webview project settings

--- src/ffi.ts
+++ src/ffi.ts
@@ -1,12 +1,12 @@
 import { CachePolicy, download, prepare } from "../deps.ts";
 
-const version = "0.7.0-dev.0";
+const version = "0.7.0-pre.0";
 const policy = Deno.env.get("PLUGIN_URL") === undefined
   ? CachePolicy.STORE
   : CachePolicy.NONE;
 
 const url = Deno.env.get("PLUGIN_URL") ??
-  `https://github.com/Snider/webview_deno/releases/download/${version}/`;
+  `https://github.com/webview/webview_deno/releases/download/${version}/`;
 
 const urls = {
   darwin: `${url}libwebview_deno.${Deno.build.arch}.dylib`,
