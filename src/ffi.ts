d47ec6d218254b9bed72d144dd23078b39da6dcb
Elias Sjögreen <elias@5monkeys.se>
2024-03-03 15:36:16 +0100

    fix: fmt

--- src/ffi.ts
+++ src/ffi.ts
@@ -3,7 +3,6 @@ import { version } from "../deno.json" with { type: "json" };
 import { dlopen, download } from "@denosaurs/plug";
 import { Webview } from "./webview.ts";
 
-
 const cache = Deno.env.get("PLUGIN_URL") === undefined ? "use" : "reloadAll";
 const url = Deno.env.get("PLUGIN_URL") ??
   `https://github.com/webview/webview_deno/releases/download/${version}/`;
