2f1de5bb78fba5ca7855d3fadb11c6ec8e173a44
Nbiba Bedis <bedisnbiba@gmail.com>
2025-01-26 09:40:20 +0100

    fixes

--- script/build.ts
+++ script/build.ts
@@ -4,6 +4,9 @@ import process from "node:process";
 const { platform } = process;
 $.setPrintCommand(true);
 
+const commonCmakeFlags =
+  "-DWEBVIEW_USE_CLANG_TIDY=OFF -DWEBVIEW_BUILD_DOCS=OFF -DWEBVIEW_USE_CLANG_FORMAT=OFF";
+
 await $.path("./build").ensureDir();
 switch (platform) {
   case "win32":
@@ -13,14 +16,14 @@ switch (platform) {
   case "linux":
     $.cd("webview");
     await $`export PATH=/usr/lib/llvm14/bin/:/usr/lib/llvm-14/bin/:/usr/lib64/llvm15/bin/:$PATH`;
-    await $`cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release -D WEBVIEW_WEBKITGTK_API=6.0 -DWEBVIEW_ENABLE_CHECKS=false -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/host-llvm.cmake`;
+    await $`cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release -D WEBVIEW_WEBKITGTK_API=6.0 -DWEBVIEW_ENABLE_CHECKS=false -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/host-llvm.cmake ${commonCmakeFlags}`;
     await $`cmake --build build`;
     await $`cp build/core/libwebview.so ../build/libwebview.${Deno.build.arch}.so`;
     await $`strip ../build/libwebview.${Deno.build.arch}.so`;
     break;
   case "darwin":
     $.cd("webview");
-    await $`cmake -G "Ninja Multi-Config" -B build -S . -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/universal-macos-llvm.cmake -DWEBVIEW_USE_CLANG_TIDY=OFF -DWEBVIEW_BUILD_DOCS=OFF -DWEBVIEW_USE_CLANG_FORMAT=OFF`;
+    await $`cmake -G "Ninja Multi-Config" -B build -S . -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/universal-macos-llvm.cmake ${commonCmakeFlags}`;
     await $`cmake --build build --config Release`;
     await $`cp build/core/Release/libwebview.dylib ../build/libwebview.${Deno.build.arch}.dylib`;
     await $`strip -x -S ../build/libwebview.${Deno.build.arch}.dylib`;
